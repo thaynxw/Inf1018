411ef9bd1705b95edb90a5246919eea1dfc5a953
thayna <thaynax342@gmail.com>
2026-09-09 19:34:52 -0300

    lab4

---
+++ lab4/ex4.c
@@ -0,0 +1,34 @@
+/* programa 1
+#include <stdio.h>
+
+int main (void) {
+  int x = 0xffffffff;
+  int y = 2;
+  printf("x=%d, y=%d\n", x, y);
+  printf("x é menor do que y? %s\n", (x<y)?"sim":"nao");
+  return 0;
+}
+*/
+/* programa 2
+#include <stdio.h>
+
+int main (void) {
+  unsigned int x = 0xffffffff;
+  unsigned int y = 2;
+  printf("x=%d, y=%d\n", x, y);
+  printf("x é menor do que y? %s\n", (x<y)?"sim":"nao");
+  return 0;
+}*/
+
+/* programa 3*/
+#include <stdio.h>
+
+int main (void) {
+  int x = 0xffffffff;
+  unsigned int y = 2;
+  printf("x=%d, y=%d\n", x, y);
+  printf("x é menor do que y? %s\n", (x<y)?"sim":"nao");
+  return 0;
+}
+
+
---
+++ lab4/ex5.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void dump(void *p, int n) {
+    unsigned char *p1 = p;
+
+    while (n--) {
+        printf("%p - %02x\n", p1, *p1);
+        p1++;
+    }
+}
+
+int main(void) {
+    signed char sc = -1;
+    unsigned int ui = sc;
+
+    printf("sc = %d\n", sc);
+    printf("ui = %u\n", ui);
+
+    printf("\ndump de ui:\n");
+    dump(&ui, sizeof(ui));
+
+    return 0;
+}
